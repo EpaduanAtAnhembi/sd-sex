e3d256b430ffa526a42e17c5c17a3429e81af474
EmersonPaduan <emerson.paduan@gmail.com>
2024-04-12 11:56:23 -0300

    aula 06 - parte2

--- aula06/tcp/client/Client.java
+++ aula06/tcp/client/Client.java
@@ -1,19 +1,43 @@
 package tcp.client;
 
+import java.io.PrintStream;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Client {
     public static void main(String[] args) {
         final String HOST = "127.0.0.1"; // localhost
         final int PORT = 9999;
         Socket socket;
+        PrintStream out;
+        Scanner teclado;
 
         try {
             socket = new Socket(HOST, PORT);
             System.out.println("Conectado com o servidor...");
+
+            out = new PrintStream(socket.getOutputStream());
+
+            teclado = new Scanner(System.in);
+
+            String msg = "";
+            while (!msg.equalsIgnoreCase("exit")) {
+                System.out.println("Digite a mensagem");
+                msg = teclado.nextLine();
+                out.println(msg);
+            }
             
         } catch (Exception e) {
             System.out.println("Erro: " + e.getMessage());
+            return;
+        }
+
+        try {
+            teclado.close();
+            out.close();
+            socket.close();
+        } catch (Exception e) {
+            // TODO: handle exception
         }
     }
 }
--- aula06/tcp/server/Server.java
+++ aula06/tcp/server/Server.java
@@ -2,25 +2,43 @@ package tcp.server;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class Server {
 
-public static void main(String[] args) {
-    final int PORT = 9999;
-    ServerSocket serverSocket;
-    Socket clientSocket;
+    public static void main(String[] args) {
+        final int PORT = 9999;
+        ServerSocket serverSocket;
+        Socket clientSocket;
+        Scanner in;
 
-    
-    try {
-        serverSocket = new ServerSocket(PORT);
-        System.out.println("Servidor disponível na porta: " + PORT);
-        System.out.println("Aguardando novo cliente...");
-        clientSocket = serverSocket.accept();
-        // ....
+        try {
+            serverSocket = new ServerSocket(PORT);
+            System.out.println("Servidor disponível na porta: " + PORT);
+            while (true) {
+                System.out.println("Aguardando novo cliente...");
+                clientSocket = serverSocket.accept();
+                in = new Scanner(clientSocket.getInputStream());
 
-    } catch (Exception e) {
-        System.out.println("Erro: " + e.getMessage());
+                String msg = "";
+                while (!msg.equalsIgnoreCase("exit")) {
+                    System.out.println("Aguardando mensagem...");
+                    msg = in.nextLine();
+                    System.out.println("Recebido: " + msg);
+                }
+            }
+        } catch (Exception e) {
+            System.out.println("Erro: " + e.getMessage());
+            return;
+        }
+
+/*         try {
+            in.close();
+            clientSocket.close();
+            serverSocket.close();
+        } catch (Exception e) {
+            // TODO: handle exception
+        } */
     }
-}
-    
+
 }
